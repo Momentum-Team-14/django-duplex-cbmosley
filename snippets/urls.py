48e863631aba81c945a0950242492b55d3040148
cbmosley <cbmosley@ncsu.edu>
2022-08-27 12:43:14 -0400

    Added the views.py, forms.py, urls.py, and necessary templates for base of my snippet webpage.

---
+++ snippets/urls.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.list_snippets, name='list_snippets'),
+    path('snippets/<int:pk>', views.snippet_detail, name='snippet_detail'),
+    path('snippets/new/', views.create_snippet, name='create_snippet'),
+    path('snippets/<int:pk>/edit/', views.snippets_edit, name='snippets_edit'),
+    path('delete_snippet/<int:pk>', views.delete_snippet, name="delete_snippet")
+]
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Snippet
+from .forms import SnippetForm
 # Create your views here.
+
+
+def list_snippets(request):
+    snippets = Snippet.objects.all()
+    return render(request, 'snippets/snippet_home.html', {"snippets": snippets})
+
+
+def snippet_detail(request, pk):
+    snippet = get_object_or_404(Snippet, pk=pk)
+    return render(request, "snippets/snippet_detail.html", {"snippet": snippet})
+
+
+def create_snippet(request):
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save()
+            return redirect('list_snippets')
+    else:
+        form = SnippetForm()
+    return render(request, 'snippets/create_snippet.html', {'form': form})
+
+
+def snippets_edit(request, pk):
+    snippet = get_object_or_404(snippet, pk=pk)
+    if request.method == "POST":
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.is_valid():
+            snippet = form.save()
+            return redirect('snippet_detail', pk=snippet.pk)
+    else:
+        form = SnippetForm(instance=snippet)
+    return render(request, 'snippets/create_snippet.html', {'form': form})
+
+
+def delete_snippet(request, pk):
+    snippet = get_object_or_404(snippet, pk=pk)
+    snippet.delete()
+    return redirect('list_snippets')
+    # return render(request, 'snippets/snippet_home.html', {"snippet": snippet})
